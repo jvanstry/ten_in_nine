c04e6140694e7d929a698bef6b0adeef18d7422c
Jerry VanStrydonck <jvanstry@gmail.com>
2014-03-27 09:38:30 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TenInNine::Application.config.secret_key_base = 'bb64a93cb2d6a1e5f129e192a2792f5cfb15519ece0682b0a3f228f86de839ae9a21a57a4432b56aa86ecf1a8a05ec72a946f54697cf597c4cef2cdc4f5a2bb3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TenInNine::Application.config.session_store :cookie_store, key: '_ten_in_nine_session'
