8530bc45995e92cdd29e7857b9ce74a7f4d8f9f4
Jerry VanStrydonck <jvanstry@gmail.com>
2014-04-03 12:14:52 -0400

    refactored project model, stylesheet fs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,4 @@
 class Project < ActiveRecord::Base
-  validates :name, :presence => true
-  validates :github_url, :presence => true
-  validates :thumbnail_url, :presence => true
-
+  validates :name, :github_url, :thumbnail_url, :presence => true
   acts_as_taggable
 end
